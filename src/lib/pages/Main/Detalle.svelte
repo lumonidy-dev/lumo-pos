bbe22849d6eabb17d1884ba9aa9d08e2b5557d08
Bruxar <larancibia@utem.cl>
2024-04-03 18:09:50 -0300

    tabla detalle pedido

--- src/lib/pages/Main/Detalle.svelte
+++ src/lib/pages/Main/Detalle.svelte
@@ -3,6 +3,10 @@
         { nombre: "Completo Italiano", cantidad: 1, precio: 12500 },
         { nombre: "Coca Cola Lata", cantidad: 1, precio: 80000 },
         { nombre: "Papas Fritas Medianas", cantidad: 1, precio: 12100 },
+        { nombre: "Papas Fritas Medianas", cantidad: 1, precio: 12100 },
+        { nombre: "Papas Fritas Medianas", cantidad: 1, precio: 12100 },
+        { nombre: "Papas Fritas Medianas", cantidad: 1, precio: 12100 },
+        { nombre: "Papas Fritas Medianas", cantidad: 1, precio: 12100 },
     ];
 
     function calcularTotal() {
@@ -18,13 +22,24 @@
     <h2>Detalle del Pedido</h2>
 
     <div class="cuadro-pedido glass-dark">
-        <ul>
-            {#each pedidos as pedido}
-                <li>
-                    {pedido.cantidad} {pedido.nombre} - ${pedido.precio}
-                </li>
-            {/each}
-        </ul>
+        <table>
+            <thead>
+                <tr>
+                    <th>Cantidad</th>
+                    <th>Nombre</th>
+                    <th>Precio</th>
+                </tr>
+            </thead>
+            <tbody>
+                {#each pedidos as pedido}
+                    <tr>
+                        <td>{pedido.cantidad}</td>
+                        <td>{pedido.nombre}</td>
+                        <td>${pedido.precio}</td>
+                    </tr>
+                {/each}
+            </tbody>
+        </table>
     </div>
     <div class="total w-60 br-20">
         <p>
@@ -37,7 +52,7 @@
 
 <style lang="scss">
     @import "src/mixins.scss";
-    
+
     .detalle {
         display: flex;
         flex-direction: column;
@@ -51,35 +66,44 @@
         border-radius: 10px;
         padding: 1px;
         width: 90%;
+        overflow-y: auto;
+        max-height: 15rem;
     }
 
-    ul {
-    /* Use flex boxes to align li items */
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    align-items: start;
-    /* Remove default padding from major browsers */
-    @include padding(2rem ,x);
-    /* Hide the default decorations for li items */
-    list-style: none;
-    & > li {
-    /* Display the elements in one line */
-    display: inline-block;
-}
-}
+    table {
+        width: 100%;
+        border-collapse: collapse; // Colapsar los bordes de la tabla
+    }
+
+    th,
+    td {
+        padding: 8px;
+        text-align: left;
+        border-right: 1px solid #fff; // Añadir línea separadora a la derecha de cada celda
+    }
+
+    th:last-child,
+    td:last-child {
+        border-right: none; // Eliminar la línea separadora en la última columna
+    }
+
+    th {
+        border-bottom: 1px solid #fff; // Añadir borde inferior a los encabezados
+    }
+
     .total {
         background-color: black;
-        margin-top:1rem;
+        margin-top: 1rem;
         padding: 1rem;
-        p{      
-                @include media-query(desktop){
-                    font-size: 2.6rem;
-                }
-                text-align:center;
-                margin: 0;
-                font-size: 1.5rem;
-                color: green;
+
+        p {
+            @include media-query(desktop) {
+                font-size: 2.6rem;
+            }
+            text-align: center;
+            margin: 0;
+            font-size: 1.5rem;
+            color: green;
         }
     }
 
